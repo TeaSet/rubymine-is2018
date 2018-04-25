6d541a360a86a6ddba75d82a93e67c7d30d63cb9
dmvo0317 <Fibonachi11235>
2018-04-25 20:32:39 +0300

    Parse the expression: added the specialized method and some fixes

--- src/main/java/com/jetbrains/python/inspection/PyConstantExpression.java
+++ src/main/java/com/jetbrains/python/inspection/PyConstantExpression.java
@@ -2,14 +2,15 @@ package com.jetbrains.python.inspection;
 
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.lang.ASTNode;
 import com.intellij.psi.*;
-import com.intellij.psi.tree.IElementType;
 import com.jetbrains.python.inspections.PyInspection;
 import com.jetbrains.python.inspections.PyInspectionVisitor;
 import com.jetbrains.python.psi.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public class PyConstantExpression extends PyInspection {
 
@@ -37,17 +38,50 @@ public class PyConstantExpression extends PyInspection {
 
         private void processIfPart(@NotNull PyIfPart pyIfPart) {
             final PyExpression condition = pyIfPart.getCondition();
+            Objects.requireNonNull(condition);
+
             if (condition instanceof PyBoolLiteralExpression) {
                 registerProblem(condition, "The condition is always " + ((PyBoolLiteralExpression) condition).getValue());
             } else if (condition instanceof PyBinaryExpression) {
-                PsiElement opSign = ((PyBinaryExpression) condition).getPsiOperator();
 
+                // >, <, ==, !=
+                Optional<PsiElement> opSign = Optional.ofNullable(((PyBinaryExpression) condition).getPsiOperator());
+                String operation;
+
+
+                //left_number -> int
                 PsiElement leftExpr = condition.getFirstChild();
                 int left = Integer.parseInt(leftExpr.getText());
 
+                //right_number -> int
                 PsiElement rightExpr = condition.getLastChild();
                 int right = Integer.parseInt(rightExpr.getText());
+
+
+                if (opSign.isPresent()) {
+                    operation = opSign.get().getText();
+                } else {
+                    throw new NullPointerException("The operational sign was not be set!");
+                }
+
+                registerProblem(condition, "The condition is always "
+                                                        + parseExpression(left, operation, right));
             }
+        }
+
+        private boolean parseExpression(int left, String operation, int right) {
+            switch (operation) {
+                //Use these types of conditional sight according to the task's rules
+                case ">":
+                    return left > right;
+                case "<":
+                    return left < right;
+                case "==":
+                    return left == right;
+                case "!=":
+                    return left != right;
+            }
+            throw new IllegalArgumentException("The conditional expression is incorrect!");
         }
     }
 }
